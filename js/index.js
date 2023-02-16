a88b6c7728f8b80df4c71e5a6566e34b96c31c6d
Dylan Stockinger <pro.stockinger@gmail.com>
2023-02-16 15:33:24 +0100

    ⚡️  toggle dark & light theme

--- js/index.js
+++ js/index.js
@@ -36,23 +36,6 @@ slideResultsContainer.forEach(function (slideResultsContainer) {
 });
 
 
-const slideContent = document.querySelector('.container-call-to-action')
-
-window.addEventListener('scroll', () => {
-
-    const {scrollTop, clientHeight} = document.documentElement;
-
-
-
-    const topElementToTopViewport = slideContent.getBoundingClientRect().top;
-
-
-
-    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.5){
-      slideContent.classList.add('active')
-    }
-})
-console.log(slideContent);
 
 const menuHamburger = document.querySelector(".menu-hamburger");
 const navLinks = document.querySelector(".nav-links");
@@ -63,3 +46,54 @@ menuHamburger.addEventListener("click", () => {
   const openStyle = document.querySelector("body");
   openStyle.classList.toggle("overflow");
 });
+
+
+
+
+
+
+
+document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
+const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+
+var theme = "dark";
+  const root = document.querySelector(":root");
+  const container = document.getElementsByClassName("theme-container")[0];
+  const themeIcon = document.getElementById("theme-icon");
+  container.addEventListener("click", setTheme);
+  function setTheme() {
+    switch (theme) {
+      case "dark":
+        setLight();
+        theme = "light";
+        break;
+      case "light":
+        setDark();
+        theme = "dark";
+        break;
+    }
+  }
+  function setLight() {
+    root.style.setProperty(
+      "--bs-dark",
+      "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
+    );
+    container.classList.remove("shadow-dark");
+    setTimeout(() => {
+      container.classList.add("shadow-light");
+      themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = sun;
+  }
+  function setDark() {
+    root.style.setProperty("--bs-dark", "#191d32");
+    container.classList.remove("shadow-light");
+    setTimeout(() => {
+      container.classList.add("shadow-dark");
+      themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = moon;
+  }
